c142a5c9c8b80ae18df0a775a2c43e5db8a89e06
Michael Lee <mikecovlee@163.com>
2018-01-06 20:55:41 +0800

    修复错误

--- deb-src/i386/make-deb.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ./usr/bin
-cp -r ../../build/bin ./usr/
-mkdir -p ./etc/covscript/imports
-cp -r ../../build/imports ./etc/covscript/
-chmod -R 755 DEBIAN
-dpkg-deb -b . covscript-1.2.1-i386.deb
