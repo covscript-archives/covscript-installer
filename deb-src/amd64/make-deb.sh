6270200a1501528359487e54a5109ea0f514bbcd
Michael Lee <mikecovlee@163.com>
2018-01-06 20:37:24 +0800

    完成Deb包制作脚本

--- deb-src/amd64/make-deb.sh
+++ deb-src/amd64/make-deb.sh
@@ -1,6 +1,6 @@
 mkdir -p ./usr/bin
-cp -r ../build/bin ./usr/bin 
+cp -r ../../build/bin ./usr/
 mkdir -p ./etc/covscript/imports
-cp -r ../build/imports ./etc/covscript/imports
+cp -r ../../build/imports ./etc/covscript/
 chmod -R 755 DEBIAN
 dpkg-deb -b . covscript-1.2.1-amd64.deb
