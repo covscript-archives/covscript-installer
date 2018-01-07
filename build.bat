fe6efedbdb53dc3e2da2a194a5cc08d14e1d79af
Michael Lee <mikecovlee@163.com>
2018-01-07 11:00:24 +0800

    更新构建脚本

--- build.bat
+++ build.bat
@@ -1,12 +1,11 @@
 @echo off
 mkdir build-cache
 cd build-cache
-set git_repo=https://github.com/covscript
-git clone %git_repo%/covscript
-git clone %git_repo%/covscript-regex
-git clone %git_repo%/covscript-darwin
-git clone %git_repo%/covscript-sqlite
-git clone %git_repo%/covscript-network
+call:git_fetch covscript
+call:git_fetch covscript-regex
+call:git_fetch covscript-darwin
+call:git_fetch covscript-sqlite
+call:git_fetch covscript-network
 xcopy /E /Y covscript\include covscript-regex\include\
 xcopy /E /Y covscript\include covscript-darwin\include\
 xcopy /E /Y covscript\include covscript-sqlite\include\
@@ -16,3 +15,15 @@ start /D covscript-regex make.bat
 start /D covscript-darwin make.bat
 start /D covscript-sqlite make.bat
 start /D covscript-network make.bat
+goto:eof
+:git_fetch
+if exist %1% (
+    cd %1%
+    git fetch
+    git pull
+    git clean -dfx
+    cd ..
+) else (
+    git clone https://github.com/covscript/%1%
+)
+goto:eof
