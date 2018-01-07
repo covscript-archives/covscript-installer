cf21ec0be24123498c72e174b4152f5efe16eaf1
Michael Lee <mikecovlee@163.com>
2018-01-07 09:05:10 +0800

    更新Linux构建脚本

--- build.sh
+++ build.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
+if [ "$#" != "1" ]; then
+    PREFIX="/usr"
+else
+    PREFIX=$1
+fi
 function start ()
 {
     cd $1
-    $2 &
+    $2
     cd ..
 }
 mkdir -p build-cache
 cd build-cache
 git_repo=https://github.com/covscript
-git clone $git_repo/covscript
-git clone $git_repo/covscript-regex
-git clone $git_repo/covscript-darwin
-git clone $git_repo/covscript-sqlite
-git clone $git_repo/covscript-network
-cp -rf covscript/include covscript-regex/
-cp -rf covscript/include covscript-darwin/
-cp -rf covscript/include covscript-sqlite/
-cp -rf covscript/include covscript-network/
-start covscript 'sh ./make.sh'
-start covscript-regex 'sh ./make.sh'
-start covscript-darwin 'sh ./make.sh'
-start covscript-sqlite 'sh ./make.sh'
-start covscript-network 'sh ./make.sh'
+function fetch_git ()
+{
+    if [ ! -d "$1" ]; then
+        git clone $git_repo/$1
+    else
+        cd $1
+        git fetch
+        git pull
+        git clean -dfx
+        cd ..
+    fi
+}
+fetch_git covscript &
+fetch_git covscript-regex &
+fetch_git covscript-darwin &
+fetch_git covscript-sqlite &
+fetch_git covscript-network &
+wait
+cp -rf covscript/include covscript-regex/ &
+cp -rf covscript/include covscript-darwin/ &
+cp -rf covscript/include covscript-sqlite/ &
+cp -rf covscript/include covscript-network/ &
+wait
+start covscript 'sh ./make.sh $PREFIX' &
+start covscript-regex 'sh ./make.sh' &
+start covscript-darwin 'sh ./make.sh' &
+start covscript-sqlite 'sh ./make.sh' &
+start covscript-network 'sh ./make.sh' &
 wait
