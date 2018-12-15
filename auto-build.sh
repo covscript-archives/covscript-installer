180fa5f6bd544259ab2de4edbfa38b06b13d849b
Michael Lee <mikecovlee@163.com>
2018-12-15 17:43:40 +0800

    Remove build scripts
    Integrate build scripts into CSBuild project

--- auto-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bash ./build.sh
-bash ./install.sh
-version_prefix=`echo "runtime.info()" | ./build/bin/cs_repl --silent | grep "Version: " | awk '{print $2}'`
-version_suffix=`echo "runtime.info()" | ./build/bin/cs_repl --silent | grep "Version: " | awk '{print $NF}'`
-architecture=`dpkg --print-architecture`
-bash ./make-deb.sh /usr $architecture ${version_prefix}"."${version_suffix}
